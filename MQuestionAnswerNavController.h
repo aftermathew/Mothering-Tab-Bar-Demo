601fdb56f5bb87c1e60880620159dd16478effc7
Demi Raven <demi@bitsyrup.com>
2009-11-23 20:58:40 -0800

    wow really working again

---
+++ MQuestionAnswerNavController.h
@@ -0,0 +1,16 @@
+//
+//  MQuestionAnswerNavController.h
+//  Mothering_TabBarDemo
+//
+//  Created by Demi Raven on 11/23/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MQuestionAnswerNavController : UINavigationController <UINavigationControllerDelegate> {
+
+}
+
+@end
